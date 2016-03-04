4fba9cc20ab8bed9cc4743dab3ed598b6bd9e911
Hugo Nguyen <hugo@scribd.com>
2016-03-04 00:10:26 -0800

    test AVPlayer

--- SmartStream/ViewController.swift
+++ SmartStream/ViewController.swift
@@ -7,10 +7,17 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     var stream: Stream?
+    var player: AVPlayer?
+    var playerItem: AVPlayerItem?
+    var playerLayer: AVPlayerLayer?
+    
+    private static var myContext = 0
+    let myContext = UnsafeMutablePointer<()>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +30,38 @@ class ViewController: UIViewController {
             if stream != nil && stream!.items.count > 0 {
                 let url = stream!.items[0].url
                 print(url!)
+                
+                let urlAsset = AVURLAsset(URL: NSURL(string: url!)!)
+                self.playerItem = AVPlayerItem(asset: urlAsset)
+                self.playerItem!.addObserver(self, forKeyPath: "status", options: [.New,.Old,.Initial], context: self.myContext)
+                
+                self.player = AVPlayer(playerItem: self.playerItem!)
+                self.playerLayer = AVPlayerLayer(player: self.player)
+                self.playerLayer!.frame = self.view.frame
+            }
+        }
+    }
+    
+    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+        if context != myContext {
+            return
+        }
+        
+        let playerItem = object as? AVPlayerItem
+        if (playerItem == nil || self.player == nil || playerItem != self.player!.currentItem) {
+            return
+        }
+        
+        if keyPath == "status" {
+            if playerItem!.status == AVPlayerItemStatus.ReadyToPlay {
+                print("ready to play")
+                
+                self.player!.play()
+                print("playing...")
+            } else if playerItem!.status == AVPlayerItemStatus.Failed {
+                print("failed to play")
+            } else {
+                print("unhandled playerItem status \(playerItem!.status)")
             }
         }
     }
