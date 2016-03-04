36578d7fc55ec31fe220826dae86eaa284520273
Hugo Nguyen <hugo@scribd.com>
2016-03-04 01:36:57 -0800

    change to use AVQueuePlayer

--- SmartStream/ViewController.swift
+++ SmartStream/ViewController.swift
@@ -11,9 +11,13 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    @IBOutlet weak var loadingLabel: UILabel!
+    
     var stream: Stream?
-    var player: AVPlayer?
-    var playerItem: AVPlayerItem?
+    var player: AVQueuePlayer?
     var playerLayer: AVPlayerLayer?
     
     private static var myContext = 0
@@ -23,21 +27,31 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view, typically from a nib.
+        loadingLabel.hidden = false
+        playButton.hidden = true
+        stopButton.hidden = true
         
         StreamClient.sharedInstance.getStream { (stream, error) -> () in
             self.stream = stream
             
             if stream != nil && stream!.items.count > 0 {
-                let url = stream!.items[0].url
-                print(url!)
                 
-                let urlAsset = AVURLAsset(URL: NSURL(string: url!)!)
-                self.playerItem = AVPlayerItem(asset: urlAsset)
-                self.playerItem!.addObserver(self, forKeyPath: "status", options: [.New,.Old,.Initial], context: self.myContext)
+                var playerItems = [AVPlayerItem]()
                 
-                self.player = AVPlayer(playerItem: self.playerItem!)
+                for item in stream!.items {
+                    let url = item.url
+                    print(url!)
+                    let urlAsset = AVURLAsset(URL: NSURL(string: url!)!)
+                    let playerItem = AVPlayerItem(asset: urlAsset)
+                    playerItem.addObserver(self, forKeyPath: "status", options: [.New,.Old,.Initial], context: self.myContext)
+                    playerItems.append(playerItem)
+                }
+                
+                self.player = AVQueuePlayer(items: playerItems)
                 self.playerLayer = AVPlayerLayer(player: self.player)
-                self.playerLayer!.frame = self.view.frame
+                self.playerLayer!.frame = self.playerView.bounds
+                
+                self.playerView.layer.addSublayer(self.playerLayer!)
             }
         }
     }
@@ -55,15 +69,24 @@ class ViewController: UIViewController {
         if keyPath == "status" {
             if playerItem!.status == AVPlayerItemStatus.ReadyToPlay {
                 print("ready to play")
-                
+                loadingLabel.hidden = true
+                playButton.hidden = false
+                stopButton.hidden = false
                 self.player!.play()
-                print("playing...")
             } else if playerItem!.status == AVPlayerItemStatus.Failed {
                 print("failed to play")
             } else {
                 print("unhandled playerItem status \(playerItem!.status)")
             }
         }
+    }
+    
+    @IBAction func onPlayTapped(sender: AnyObject) {
+        self.player?.play()
+    }
+    
+    @IBAction func onStopTapped(sender: AnyObject) {
+        self.player?.pause()
     }
 
 }
