b8ead0878fc3849853ff741438025f75d12cc294
Marc A. Anderson <marc@marcadam.com>
2016-03-10 15:03:07 -0800

    Display page view in paging scroll view

--- ChannelCollectionPagingViewCell.swift
+++ ChannelCollectionPagingViewCell.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ChannelCollectionPagingViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var pageControl: UIPageControl!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+        let scrollViewWidth = UIScreen.mainScreen().bounds.width
+        scrollView.frame = CGRect(x: 0, y: 0, width: scrollViewWidth, height: 200)
 
+        let pageWidth = scrollView.bounds.width
+        let pageHeight = scrollView.bounds.height
+        scrollView.contentSize = CGSize(width: pageWidth * 3, height: pageHeight)
+
+        for index in 0..<3 {
+            let pageView = ChannelCollectionPageView(frame: CGRect(x: (pageWidth * CGFloat(index)), y: 0, width: pageWidth, height: pageHeight))
+            scrollView.addSubview(pageView)
+        }
+    }
 }
