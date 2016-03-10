fabff2f601e064b7d2ff3ba9dd038ac0255b1ffd
Marc A. Anderson <marc@marcadam.com>
2016-03-10 11:34:46 -0800

    Add collection paging cell and content cell

---
+++ ChannelCollectionPageView.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelCollectionPageView.swift
+//  SmartStream
+//
+//  Created by Marc Anderson on 3/10/16.
+//  Copyright © 2016 SmartStream. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCollectionPageView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ChannelCollectionPagingViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelCollectionPagingViewCell.swift
+//  SmartStream
+//
+//  Created by Marc Anderson on 3/10/16.
+//  Copyright © 2016 SmartStream. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCollectionPagingViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
