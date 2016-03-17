0c299d8a76768cb838963063918c1b2866aaebe3
jerlao <jerlao@gmail.com>
2016-03-16 20:02:12 -0700

    update explore header section

---
+++ ExploreSectionCollectionReusableView.swift
@@ -0,0 +1,16 @@
+//
+//  ExploreSectionCollectionReusableView.swift
+//  SmartStream
+//
+//  Created by Jerry on 3/16/16.
+//  Copyright © 2016 SmartStream. All rights reserved.
+//
+
+import UIKit
+
+class ExploreSectionCollectionReusableView: UICollectionReusableView {
+    
+    @IBOutlet weak var sectionHeaderLabel: UILabel!
+    
+    
+}
