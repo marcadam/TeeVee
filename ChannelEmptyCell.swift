662dcf4212cd93ae7eeb4a23d4c5eb09f5302c5b
jerlao <jerlao@gmail.com>
2016-04-04 13:50:32 -0700

    add check to empty state on cell pressed

---
+++ ChannelEmptyCell.swift
@@ -0,0 +1,67 @@
+//
+//  ChannelCollectionViewCell
+//  SmartChannel
+//
+//  Created by Jerry on 3/5/16.
+//  Copyright © 2016 SmartChannel. All rights reserved.
+//
+
+import UIKit
+
+class ChannelEmptyCell: UICollectionViewCell {
+    
+    @IBOutlet weak var channelImageView: UIImageView!
+    @IBOutlet weak var infoContainerView: UIView!
+    @IBOutlet weak var channelNameLabel: UILabel!
+    @IBOutlet weak var checkView: UIView!
+    @IBOutlet weak var CheckImageView: UIImageView!
+
+    var isSelected: Bool? {
+        didSet {
+            if let selected = isSelected {
+                if selected {
+                    UIView.animateWithDuration(0.3, animations: {
+                        self.checkView.layer.opacity = 0.8
+                        self.CheckImageView.layer.opacity = 1
+                    })
+                } else {
+                    UIView.animateWithDuration(0.3, animations: {
+                        self.checkView.layer.opacity = 0
+                        self.CheckImageView.layer.opacity = 0
+                    })
+                }
+            }
+        }
+    }
+    
+    var channel: Channel! {
+        didSet {
+            channelNameLabel.text = channel.title
+            if let thumbnail = channel.thumbnail_url {
+                let request = NSURLRequest(URL: NSURL(string: thumbnail)!)
+                channelImageView.setImageWithURLRequest(request, placeholderImage: UIImage(named: "placeholder"), success: { (request: NSURLRequest, response: NSHTTPURLResponse?, image: UIImage) -> Void in
+                    self.channelImageView.image = image
+                    self.channelImageView.layer.opacity = 0
+                    UIView.transitionWithView(self.channelImageView, duration: 0.3, options: UIViewAnimationOptions.CurveEaseIn, animations: { () -> Void in
+                        self.channelImageView.layer.opacity = 1
+                        }, completion: { (bool: Bool) -> Void in
+                            //
+                    })
+                    }, failure: { (request: NSURLRequest, response: NSHTTPURLResponse?, error: NSError) -> Void in
+                        //
+                })
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        backgroundColor = Theme.Colors.LightBackgroundColor.color
+        infoContainerView.backgroundColor = Theme.Colors.DarkBackgroundColor.color
+        channelNameLabel.textColor = Theme.Colors.HighlightColor.color
+        checkView.layer.opacity = 0
+        checkView.backgroundColor = Theme.Colors.LightButtonColor.color
+        CheckImageView.layer.opacity = 0
+    }
+}
--- EmptyChannelTableViewCell.swift
+++ EmptyChannelTableViewCell.swift
@@ -21,7 +21,7 @@ class EmptyChannelTableViewCell: UITableViewCell {
         }
     }
     private let bgColor = Theme.Colors.BackgroundColor.color
-    private let cellID = "com.teevee.ChannelCollectionViewCell"
+    private let cellID = "com.teevee.ChannelEmptyCell"
     var selectedChannels = [String]()
     
     weak var delegate: EmptyChannelDelegate?
@@ -33,10 +33,11 @@ class EmptyChannelTableViewCell: UITableViewCell {
         // Initialization code
         collectionView.delegate = self
         collectionView.dataSource = self
-        let channelCellNIB = UINib(nibName: "ChannelCollectionViewCell", bundle: NSBundle.mainBundle())
+        let channelCellNIB = UINib(nibName: "ChannelEmptyCell", bundle: NSBundle.mainBundle())
         collectionView.registerNib(channelCellNIB, forCellWithReuseIdentifier: cellID)
         collectionView.backgroundColor = bgColor
         collectionView.allowsMultipleSelection = true
+        collectionView.tintColor = UIColor.whiteColor()
     }
     
     override func setSelected(selected: Bool, animated: Bool) {
@@ -62,7 +63,7 @@ extension EmptyChannelTableViewCell: UICollectionViewDelegate, UICollectionViewD
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellID, forIndexPath: indexPath) as! ChannelCollectionViewCell
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellID, forIndexPath: indexPath) as! ChannelEmptyCell
         if let channels = featuredChannels {
             cell.channel = channels[indexPath.item]
         }
@@ -75,6 +76,9 @@ extension EmptyChannelTableViewCell: UICollectionViewDelegate, UICollectionViewD
             let channel = channels[indexPath.item]
             selectedChannels.append(channel.channel_id!)
             delegate?.emptyChannel(self, didUpdateSelectedChannels: selectedChannels)
+            
+            let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ChannelEmptyCell
+            cell.isSelected = true
         }
     }
     
@@ -86,6 +90,9 @@ extension EmptyChannelTableViewCell: UICollectionViewDelegate, UICollectionViewD
                     if selected.channel_id == channelID {
                         selectedChannels.removeAtIndex(index)
                         delegate?.emptyChannel(self, didUpdateSelectedChannels: selectedChannels)
+                        
+                        let cell = collectionView.cellForItemAtIndexPath(indexPath) as! ChannelEmptyCell
+                        cell.isSelected = false
                         return
                     }
                 }
